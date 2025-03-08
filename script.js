944a071f49cae474922fcbe406d862728a4983f8
PPark8093 <parkjuwon8093@outlook.kr>
2025-03-08 23:33:44 +0900

    first_fix1

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const observer = new IntersectionObserver((entities, observer) => {
-    entities.forEach(entry => {
-        if (entry.isIntersecting)
-        {
-            document.querySelector("header").style.backgroundColor = "white";
-            document.querySelectorAll(".header_button").forEach(element => {
-                element.style.color = "black";
-            });
-
-            document.querySelector(".header_icon").style.display = "block";
-        }
-        else
-        {
-            document.querySelector("header").style.backgroundColor = "transparent";
-            document.querySelectorAll(".header_button").forEach(element => {
-                element.style.color = "white";
-            });
-
-            document.querySelector(".header_icon").style.display = "none";
-        }
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        document.querySelector("header").style.backgroundColor = "#fff";
+        document.querySelector(".header_buttons").style.marginLeft = "2rem"
+        document.querySelectorAll(".header_button").forEach(el => {
+          el.style.color = "#000";
+        });
+        document.querySelector(".header_icon").style.display = "block";
+      } else {
+        document.querySelector("header").style.backgroundColor = "transparent";
+        document.querySelectorAll(".header_button").forEach(el => {
+          el.style.color = "#fff";
+        });
+        document.querySelector(".header_icon").style.display = "none";
+      }
     });
-}, { })
-
-observer.observe(document.querySelector("#main_about"));
+  }, { threshold: 0.6 });
+  
+  observer.observe(document.querySelector("#introduce_background"));
+  
